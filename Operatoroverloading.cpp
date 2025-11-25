92e3fad9c6053b8c6792a5083af9773395585b69
savarirokade-svg <savarirokade@gmail.com>
2025-11-25 22:58:43 +0530

    Create Operatoroverloading.cpp

---
+++ Operatoroverloading.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    int real, imag;
+
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    // Operator Overloading +
+    Complex operator + (const Complex &c) {
+        return Complex(real + c.real, imag + c.imag);
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(5, 3), c2(2, 4);
+    Complex c3 = c1 + c2;   // Using overloaded operator
+    c3.display();
+}
